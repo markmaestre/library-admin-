4eaf89712eb826e3c470c9f4fac890416ee73a80
markmaestre <markranier.maestre@tup.edu.ph>
2025-11-24 08:10:44 +0800

    deploy

--- web/src/components/Utils/Api.js
+++ web/src/components/Utils/Api.js
@@ -1,8 +1,8 @@
-// const API_URL = "https://library-admin-rdaq.onrender.com";
-
-// export default API_URL;
-
-
-const API_URL = "http://192.168.1.44:10000";
+const API_URL = "https://libraryadmin.onrender.com";
 
 export default API_URL;
+
+
+// const API_URL = "http://192.168.1.44:10000";
+
+// export default API_URL;
